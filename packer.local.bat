8d4548bb048b2424231dee80c461f5c14886f8c7
BigNoid <big.noid1979@gmail.com>
2013-03-21 14:11:20 +0100

    Added screen calibration

---
+++ packer.local.bat
@@ -0,0 +1,3 @@
+echo Build main Textures.xbt -------------------------------------------------
+rem START /B /WAIT D:\XBMC\Source\xbmc\tools\TexturePacker\TexturePacker.exe -dupecheck -input media\ -output media\Textures.xbt
+START /B /WAIT D:\XBMC\Source\xbmc\tools\TexturePacker\TexturePacker.exe -dupecheck -input media\ -output Z:\rutge_000\AppData\Roaming\XBMC\addons\skin.droid\media\Textures.xbt
